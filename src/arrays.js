3ca8eec482da8d3442ba2d0d829431ed6e7f4e8b
josephcruel <Joseph-Cruel@live.nmit.ac.nz>
2023-05-16 10:50:41 +1200

    Completed third example

--- src/arrays.js
+++ src/arrays.js
@@ -26,18 +26,51 @@
 
 //NOTE - twoArrays([0,0]),([0,1]) -> 3
 
-function twoArrays(arr) {
-    let num = 0
+// function twoArrays(arr) {
+//     let num = 0
 
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr[i].length; j++) {
-            if (arr[i][j] === 0) {
-                num++
+//     for (let i = 0; i < arr.length; i++) {
+//         for (let j = 0; j < arr[i].length; j++) {
+//             if (arr[i][j] === 0) {
+//                 num++
+//             }
+//         }
+//     }
+//     return num
+// }
+
+// console.log(twoArrays([0,0],[0,0]))
+    
+
+//TODO - Write a function the returns the largest even number in an array.
+
+//NOTE - evenLargestNumber([1,10,17]) -> 10
+//NOTE - evenLargestNumber([1,3,5,7,9] -> "No even number"
+
+// //How i did it
+// function evenLargestNumber(arr) {
+//     let num = 0
+
+//     for(let i = 0; i < arr.length; i++) {
+//         if (arr[i]) % 2 == 0  {
+//             return num 
+//         }       
+//     }
+//     return "No even number"
+// } 
+
+//How it is done 
+function evenLargestNumber(arr) {
+    let benchmark = -1 
+
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] % 2 === 0) {
+            if(arr[i] > benchmark) {
+                benchmark = arr[i]
             }
         }
     }
-    return num
+    return benchmark 
 }
 
-console.log(twoArrays([0,0],[0,0]))
-    
+console.log(evenLargestNumber([1,2,3,4,5,10,1000]))
